c7ab6fd0765e78f71424381627a945c2f9dccecc
KimYoungJune <hot6tweety@gmail.com>
2025-12-24 23:50:30 +0900

    leetcode/125: Valid Palindrome

---
+++ src/string/palindrome/Palindrome.java
@@ -0,0 +1,24 @@
+package string.palindrome;
+
+public class Palindrome {
+
+    public boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (!Character.isLetterOrDigit(str.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(str.charAt(end))) {
+                end--;
+            } else {
+                if (Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end))) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
