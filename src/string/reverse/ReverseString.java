3691f1bd4d7273ecb9db7f4258065f2e54f5d2b2
KimYoungJune <hot6tweety@gmail.com>
2025-12-25 13:28:53 +0900

    leetcode/344/: Reverse String

---
+++ src/string/reverse/ReverseString.java
@@ -0,0 +1,20 @@
+package string.reverse;
+
+public class ReverseString {
+
+    public char[] reverseString(char[] str) {
+        int start = 0;
+        int end = str.length - 1;
+
+        while (start < end)  {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return str;
+    }
+}
---
+++ src/string/reverse/ReverseString.kt
@@ -0,0 +1,27 @@
+package string.reverse
+
+fun reverseString(s: CharArray): String {
+//    var start = 0
+//    var end = s.size - 1
+//
+//    while (start < end) {
+//        s[start] = s[end].also { s[end] = s[start] }
+//        start++
+//        end--
+//    }
+
+    /* reverse logic
+
+    val midPoint = (size / 2) - 1
+    if (midPoint < 0) return
+    var reverseIndex = lastIndex
+    for (index in 0..midPoint) {
+        val tmp = this[index]
+        this[index] = this[reverseIndex]
+        this[reverseIndex] = tmp
+        reverseIndex--
+    }
+     */
+    return s.reverse().toString()
+
+}
